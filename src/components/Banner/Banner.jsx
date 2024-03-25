edf98fa171ff792883d509414dd9aad45b9e32a3
smais007 <smaisshawon@gmail.com>
2024-03-25 20:42:45 +0600

    nav and banner section added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+const Banner = () => {
+  return (
+    <div className="container w-[75rem] mx-auto bg-[#1313130D] rounded-3xl flex justify-between items-center px-[120px] py-20">
+      <div>
+        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo?</h1>
+        <button>Book List</button>
+      </div>
+      <div>
+        <img src="/src/images/pngwing 1.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
